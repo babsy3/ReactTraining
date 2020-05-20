4a868f87e7c9d9218e8b8a536a8f56696510d324
Babsy V. Babu <babsy3@gmail.com>
2020-05-20 13:23:36 +0530

    initial comment

---
+++ src/components/Greet.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Greet=(props)=>{
+    return <h1>Hello {props.name} aka {props.heroName}!!!</h1>
+}
+export default Greet
---
+++ src/components/Message.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class Welcome extends Component{
+    constructor(){
+        super()
+        this.state={
+            message: 'Welcome visitor!'
+        }
+    }
+    changeMessage=()=>{
+        this.setState({
+            message:'Thank you for subscribing!'
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={()=>this.changeMessage()}>Click Me!</button>
+            </div>
+        )
+    }
+}
+export default Welcome
